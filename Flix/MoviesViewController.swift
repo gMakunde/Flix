1dc33968b2e0bd6beecd9d03b330c21333ad2b88
George Martin <georgeg10live@gmail.com>
2020-01-29 23:40:48 -0500

    yahhh

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -49,11 +50,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
-        cell.textLabel?.text = title
+        cell.titleLabel.text = title
+        let synopsis = movie["overview"] as! String
+        cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterURL!)
         return cell
         
     }
