da427aae9309fc3c137f69f43e35cb28b2961b56
George Martin <georgeg10live@gmail.com>
2020-01-29 21:22:42 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by George Martin on 11/9/1398 AP.
+//  Copyright © 1398 Codepath project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
